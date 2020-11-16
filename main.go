b75b306a85583f5e32dca0d6a9aa1b42cb96afbe
Lauren Oden <laurenoden97@gmail.com>
2020-11-16 04:42:34 -0500

    fixed error in new git deploy

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func main() {
 
 func init() {
 	opts := renderer.Options{
-		ParseGlobPattern: "github.com/lhoden/goNewsAggregator/tpl/*.html",
+		ParseGlobPattern: "src/github.com/lhoden/goNewsAggregator/tpl/*.html",
 	}
 
 	rnd = renderer.New(opts)
